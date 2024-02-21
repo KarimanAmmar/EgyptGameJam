c1cd02993ffc402f612e2416b89e76960bf95d7b
NayraYehya <159999369+NayraYehya@users.noreply.github.com>
2024-02-21 17:54:37 +0200

    loadScreen edits

--- Assets/UI/UI_Scripts/N_MainMenu.cs
+++ Assets/UI/UI_Scripts/N_MainMenu.cs
@@ -2,6 +2,7 @@ using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class N_MainMenu : MonoBehaviour
@@ -9,9 +10,26 @@ public class N_MainMenu : MonoBehaviour
     [SerializeField] private RectTransform Panel;
     [SerializeField] private float topPosY, CenterPosY;
     [SerializeField] private float tweenDuration;
+    [SerializeField] private Slider loadingslider;
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        /*mainmenu.SetActive(false);
+        loadingScreen.SetActive(true);*/
+        StartCoroutine(LoadingLevel(1));
+        //SceneManager.LoadScene(1);
+    }
+
+    IEnumerator LoadingLevel(int level)
+    {
+        AsyncOperation loadoperation = SceneManager.LoadSceneAsync(level);
+
+        while (!loadoperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadoperation.progress / 0.9f);
+            loadingslider.value = progressValue;
+            yield return null;
+        }
     }
     public void Quit()
     {

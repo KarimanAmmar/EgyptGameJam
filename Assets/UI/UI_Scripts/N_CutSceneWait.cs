a8e259876f13f933c8dce00d1b68a6429735ff91
NayraYehya <159999369+NayraYehya@users.noreply.github.com>
2024-02-22 19:14:15 +0200

    CutScene create video

---
+++ Assets/UI/UI_Scripts/N_CutSceneWait.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class N_CutSceneWait : MonoBehaviour
+{
+    public float waitTime = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startIntro());
+    }
+
+    IEnumerator startIntro()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(1);
+    }
+}

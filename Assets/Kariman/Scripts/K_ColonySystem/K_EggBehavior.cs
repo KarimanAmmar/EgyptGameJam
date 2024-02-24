72c08eecad6a72779b5287595de9ca965c5e73b5
KarimanAmmar <76701636+KarimanAmmar@users.noreply.github.com>
2024-02-24 09:29:40 +0200

    Egg collects and enemies power changed

--- Assets/Kariman/Scripts/K_ColonySystem/K_EggBehavior.cs
+++ Assets/Kariman/Scripts/K_ColonySystem/K_EggBehavior.cs
@@ -7,10 +7,9 @@ public class K_EggBehavior : MonoBehaviour
     [SerializeField] GameEvents eggEvent;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Pigeon") ||
-            other.gameObject.layer == LayerMask.NameToLayer("MainPigeon"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("MainPigeon"))
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject);    
             eggEvent.GameAction?.Invoke();
         }
     }

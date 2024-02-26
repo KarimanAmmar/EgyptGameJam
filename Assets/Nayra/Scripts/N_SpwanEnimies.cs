3d51d0c3dc0e4f2204ceb896e9e82a62ae768370
KarimanAmmar <76701636+KarimanAmmar@users.noreply.github.com>
2024-02-26 12:52:46 +0200

    spawnRate logic changed

--- Assets/Nayra/Scripts/N_SpwanEnimies.cs
+++ Assets/Nayra/Scripts/N_SpwanEnimies.cs
@@ -20,7 +20,7 @@ public class N_SpwanEnimies : MonoBehaviour
     [SerializeField] private int maxY = 4;
     [SerializeField] private float valX = 10f;
 
-    [SerializeField] private float spwanRate;
+    [SerializeField] private float spwanRate = 1.5f;
     [SerializeField] private int waveTime = 20;
 
     [SerializeField] GameEvents _checkEnemy;
@@ -35,7 +35,8 @@ public class N_SpwanEnimies : MonoBehaviour
 
     private void Start()
     {
-        InvokeRepeating("SpawnEnemy", 2f, spwanRate);
+        StartCoroutine(SpawnEnemy());
+        StartCoroutine(spwanratechange());
     }
 
     private void Update()
@@ -47,37 +48,52 @@ public class N_SpwanEnimies : MonoBehaviour
         //Debug.Log(Time.time);
     }
 
-    private void SpawnEnemy()
+    IEnumerator SpawnEnemy()
     {
-
-        randomPrefab = enemyPrefab[Random.Range(0, enemyCount+1)];
-
-        float randomY = Random.Range(minY, maxY);
-        Vector3 randomPosition = new Vector3(valX, randomY, 0f);
-
-        // Log the spawn information
-        //Debug.Log("Spawned enemy prefab: " + randomPrefab.name + " at position: " + randomPosition);
-        namePrefab = randomPrefab.name;
-
-
-        //enemy = Instantiate(randomPrefab, randomPosition, Quaternion.identity,this.transform);
-        enemy = Instantiate(randomPrefab, randomPosition, Quaternion.Euler(0f,0f,90f) ,this.transform);
-        enemyData = enemy.GetComponent<N_EnemyData>();
-
-        /*float randomY = Random.Range(Screen.height, 0);
-        Vector3 randomScreenPosition = new Vector3(Screen.width, randomY, 10);
-        Vector3 randomPosition = Camera.main.ScreenToWorldPoint(randomScreenPosition);
-        enemy = Instantiate(randomPrefab, randomPosition, Quaternion.identity);*/
-
-        // Add velocity to the enemy
-        Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
-        if (rb != null)
+        while (true)
         {
-            //float randomSpeed = Random.Range(1f, 5f);
-            //rb.velocity = new Vector2(-randomSpeed , 0f);
-            rb.velocity = new Vector2(- enemyData._speed, 0f);
-        }
+            yield return new WaitForSeconds(spwanRate);
+            randomPrefab = enemyPrefab[Random.Range(0, enemyCount + 1)];
 
-        _checkEnemy.GameAction?.Invoke();
+            float randomY = Random.Range(minY, maxY);
+            Vector3 randomPosition = new Vector3(valX, randomY, 0f);
+
+            // Log the spawn information
+            //Debug.Log("Spawned enemy prefab: " + randomPrefab.name + " at position: " + randomPosition);
+            namePrefab = randomPrefab.name;
+
+
+            //enemy = Instantiate(randomPrefab, randomPosition, Quaternion.identity,this.transform);
+            enemy = Instantiate(randomPrefab, randomPosition, Quaternion.Euler(0f, 0f, 90f), this.transform);
+            enemyData = enemy.GetComponent<N_EnemyData>();
+
+            /*float randomY = Random.Range(Screen.height, 0);
+            Vector3 randomScreenPosition = new Vector3(Screen.width, randomY, 10);
+            Vector3 randomPosition = Camera.main.ScreenToWorldPoint(randomScreenPosition);
+            enemy = Instantiate(randomPrefab, randomPosition, Quaternion.identity);*/
+
+            // Add velocity to the enemy
+            Rigidbody2D rb = enemy.GetComponent<Rigidbody2D>();
+            if (rb != null)
+            {
+                //float randomSpeed = Random.Range(1f, 5f);
+                //rb.velocity = new Vector2(-randomSpeed , 0f);
+                rb.velocity = new Vector2(-enemyData._speed, 0f);
+            }
+            _checkEnemy.GameAction?.Invoke();
+            Debug.Log("1.5 =" +spwanRate);
+        }
+    }
+    IEnumerator spwanratechange()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(20);
+            if(spwanRate > 0.8)
+            {
+                spwanRate -= 0.05f;
+            }
+            Debug.Log("20 =" + spwanRate);
+        }
     }
 }
